397dbdfe3610d75c22cd438917316a3ada31691e
Hugh Guiney <hugh@hughguiney.com>
2019-05-02 16:33:41 -0400

    project setup

---
+++ qr-app/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Homepage</h2>
+    </div>
+  );
+}
+
+export default App;
